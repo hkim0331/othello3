7acc9636d7d59be5418eab6414660392b099f765
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2017-12-05 13:54:24 +0900

    update replay.rkt
    -s num で手の間に num 秒スリープする。
    num は小数点ついても大丈夫。

--- replay.rkt
+++ replay.rkt
@@ -1,11 +1,15 @@
+#!/usr/bin/env racket
 #lang racket/gui
 ;; リプレーする棋譜をコマンドラインから取れるように。
 ;; FIXME: 結果を表示するか？
+;; sleep を引数で渡す。
 
 (require "display.rkt"
          "hand.rkt"
          "board.rkt"
          "turn.rkt")
+
+(define *sleep* 0)
 
 (define init
   (lambda (n)
@@ -26,12 +30,21 @@
                     (y (third hand)))
                 (hand! x y m)
                 (display)
-                (sleep 0.5)))
+                (sleep *sleep*)))
             (loop (read in))))))))
 
-;;引数をベクタで返す。
-;;(current-command-line-arguments)
-;;なので、
+;;; $ ./replay -s 0.5 records/*.txt
+
+(define (args xs)
+  (define (A args ret)
+    (cond
+      ((null? args) (reverse ret))
+      ((string=? (car args) "-s")
+       (set! *sleep* (string->number (cadr args)))
+       (A (cddr args) ret))
+      (else
+       (A (cdr args) (cons (car args) ret)))))
+  (A xs '()))
 
 (map replay
-     (vector->list (current-command-line-arguments)))
+     (args (vector->list (current-command-line-arguments))))
